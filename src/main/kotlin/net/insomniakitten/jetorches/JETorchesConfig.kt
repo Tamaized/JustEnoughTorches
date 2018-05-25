ee2beb2f7611253e3e5171e8b3f524a172f0a517
InsomniaKitten <admin@designdevils.co.uk>
2018-05-25 14:02:35 +0100

    Config changes

--- src/main/kotlin/net/insomniakitten/jetorches/JETorchesConfig.kt
+++ src/main/kotlin/net/insomniakitten/jetorches/JETorchesConfig.kt
@@ -6,30 +6,24 @@ import net.minecraftforge.common.config.Config.Name
 import net.minecraftforge.common.config.Config.RequiresMcRestart
 import net.minecraftforge.common.config.Config.RequiresWorldRestart
 import net.minecraftforge.common.config.Config.Type
-import net.minecraftforge.common.config.ConfigManager
+import net.minecraftforge.common.config.ConfigManager.sync
 import net.minecraftforge.fml.client.event.ConfigChangedEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
 @Config(modid = JETorches.ID, name = JETorches.ID)
-class JETorchesConfig private constructor() {
+object JETorchesConfig {
+    @Name("underwater_torches")
+    @Comment("If true, the prismarine torch will not break when placed in water.")
+    @RequiresMcRestart
+    var prismarineUnderwater = true
+
+    @Name("colored_lighting")
+    @Comment("If true, torches will produce colored lighting when Mirage/Albedo is present.")
+    @RequiresWorldRestart
+    var coloredLighting = true
+
     @SubscribeEvent
     fun onConfigChanged(event: ConfigChangedEvent.OnConfigChangedEvent) {
-        if (JETorches.ID == event.modID) {
-            ConfigManager.sync(JETorches.ID, Type.INSTANCE)
-        }
-    }
-
-    companion object {
-        @Name("underwater_torches")
-        @Comment("If true, the prismarine torch will not break when placed in water.")
-        @RequiresMcRestart
-        @JvmField
-        var prismarineUnderwater = true
-
-        @Name("colored_lighting")
-        @Comment("If true, torches will produce colored lighting when Mirage/Albedo is present.")
-        @RequiresWorldRestart
-        @JvmField
-        var coloredLighting = true
+        if (JETorches.ID == event.modID) sync(JETorches.ID, Type.INSTANCE)
     }
 }
